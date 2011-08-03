f03a43e42f5b4388b32affaac70976708aaab794
Vadim Sukhoykin <vadim@immo.ru>
2011-08-03 16:58:26 +0400

    -layout fix;

--- htdocs/js/fm-player.js
+++ htdocs/js/fm-player.js
@@ -7,7 +7,84 @@ function debug(str)
 	$('.debug').append(str + '<br />');
 }
 
+function audioDebug(audio)
+{
+	$(audio).bind('playing', function(){
+		debug('playing');
+	});
+	
+	$(audio).bind('abort', function(){
+		debug('abort');
+	});
+	
+	$(audio).bind('canplay', function(){
+		debug('canplay: ' + this.buffered.length);
+	});
+	
+	$(audio).bind('durationchange', function(){
+		debug('durationchange: ' + this.duration);
+	});
+	
+	$(audio).bind('emptied', function(){
+		debug('emptied');
+	});
+	
+	$(audio).bind('ended', function(){
+		debug('ended');
+	});
+	
+	$(audio).bind('error', function(){
+		debug('error: ' + this.error.code);
+	});
+	
+	$(audio).bind('loadeddata', function(){
+		debug('loadeddata');
+	});
+	
+	$(audio).bind('loadedmetadata', function(e){
+		debug('loadedmetadata');
+	});
+	
+	$(audio).bind('loadstart', function(){
+		debug('loadstart');
+	});
+	
+	$(audio).bind('ratechange', function(){
+		debug('ratechange');
+	});
+	
+	$(audio).bind('readystatechange', function(){
+		debug('readystatechange');
+	});
+	
+	$(audio).bind('seeked', function(){
+		debug('seeked');
+	});
+	
+	$(audio).bind('seeking', function(){
+		debug('seeking');
+	});
+	
+	$(audio).bind('stalled', function(){
+		debug('stalled');
+	});
+	
+	$(audio).bind('suspend', function(){
+		debug('suspend');
+	});
+	
+	$(audio).bind('volumechange', function(){
+		debug('volumechange');
+	});
+	
+	$(audio).bind('waiting', function(){
+		debug('waiting');
+	});
+}
+
 /**
+ * TODO: create an object for player + cache selector elements
+ * TODO: player must preload next track in a list while current is playing
  * TODO: player has to continue play list if stop button was not clicked
  */
 
@@ -17,130 +94,47 @@ function playPause(button)
 		
 		$('#audio-container').html('<audio id="audio" preload="metadata" autobuffer></audio>');
 		
-		// HTML5 <audio>
-		audio = $('#audio')[0];
-		
-		$(audio).bind('abort', function(){
-			debug('abort');
-		});
-		
-		$(audio).bind('canplay', function(){
-			debug('canplay: ' + this.buffered.length);
-		});
-		
-		$(audio).bind('canplaythrough', function(){
-			debug('canplaythrough: ' + this.buffered.length);
-			this.play();
-		});
-		
-		$(audio).bind('durationchange', function(){
-			debug('durationchange: ' + this.duration);
-		});
-		
-		$(audio).bind('emptied', function(){
-			debug('emptied');
-		});
-		
-		$(audio).bind('ended', function(){
-			debug('ended');
-		});
-		
-		$(audio).bind('error', function(){
-			debug('error: ' + this.error.code);
-		});
-		
-		$(audio).bind('loadeddata', function(){
-			debug('loadeddata: ' + this.buffered.length);
-		});
-		
-		$(audio).bind('loadedmetadata', function(e){
-			debug('loadedmetadata');
-			debug('currentTime:' + this.currentTime);
-			debug('initialTime:' + this.initialTime);
-			debug('defaultPlaybackRate:' + this.defaultPlaybackRate);
-			debug('playbackRate:' + this.playbackRate);
-		});
-		
-		$(audio).bind('loadstart', function(){
-			debug('loadstart');
-		});
-		
-		$(audio).bind('pause', function(){
-			debug('pause');
-		});
-		
-		$(audio).bind('play', function(){
-			debug('play');
-		});
-		
-		$(audio).bind('playing', function(){
-			debug('playing');
-		});
+		// HTML5 <audio> support:
+		audio = $('#audio').get(0);
+		// else Flash support.
 		
 		$(audio).bind('progress', function(){
-			debug('progress: ' + this.buffered.end(0));
-			
-		});
-		
-		$(audio).bind('ratechange', function(){
-			debug('ratechange');
-		});
-		
-		$(audio).bind('readystatechange', function(){
-			debug('readystatechange');
-		});
-		
-		$(audio).bind('seeked', function(){
-			debug('seeked');
-		});
-		
-		$(audio).bind('seeking', function(){
-			debug('seeking');
-		});
-		
-		$(audio).bind('stalled', function(){
-			debug('stalled');
-		});
-		
-		$(audio).bind('suspend', function(){
-			debug('suspend');
+			$('#loaded').css('width', parseInt(this.buffered.end(0) / this.duration * $('#bar').width()) + 'px');
 		});
 		
 		$(audio).bind('timeupdate', function(){
-			//debug('timeupdate');
+			$('#played').css('width', parseInt(this.currentTime / this.duration * $('#bar').width()) + 'px');
 		});
 		
-		$(audio).bind('volumechange', function(){
-			debug('volumechange');
+		$(audio).bind('canplaythrough', function(){
+			this.play();
 		});
 		
-		$(audio).bind('waiting', function(){
-			debug('waiting');
+		$(audio).bind('play', function(){
+			button.addClass('pause');
 		});
+		
+		$(audio).bind('pause', function(){
+			button.removeClass('pause');
+		});
+		
+		//audioDebug(audio);
 		
 		//audio.src = '../mp3/sample.mp3';
 		audio.src = '../mp3/stream.mp3';
 		
-		//audio.play();
-		
-		button.addClass('pause');
-	}
-	/*
-	if (!fm_is_play) {
-		
-		fm_is_play = true;
-		
-		
-		
-		
-		
-		button.addClass('pause');
-		
 	} else {
 		
-		fm_is_play = false;
-		button.removeClass('pause');
-	}*/
+		// HTML5 <audio> support:
+		audio = $('#audio').get(0);
+		// else Flash support.
+		
+		if (audio.paused) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	}
 }
 
 $(document).ready(function(){
